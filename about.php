18cb04860472731c9e968eecaf89097e7ae8d56b
Gunal K <146356261+Gunal-k@users.noreply.github.com>
2024-10-24 18:48:09 +0530

    Add files via upload

---
+++ about.php
@@ -0,0 +1,39 @@
+
+<?php
+    include("Header.html");
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="styles/about.css">
+    <title>About - Bangalore University</title>
+</head>
+<body>
+    <div class="about">
+        <img src="assets/clg2.jpg" alt="">
+        <h1>About Us</h1>
+        <div class="word_wrap">
+            <p>
+            Bangalore University, established in July 1964, stands as a significant landmark in India's higher education landscape, particularly within the vibrant and rapidly evolving city of Bangalore, often referred to as the "I.T. Capital of India." The university emerged as an offshoot of the University of Mysore, with the primary goal of accommodating the growing need for educational institutions within the metropolitan area and its neighboring districts, including Bangalore, Kolar, and Tumkur. The institution began with a strong foundation, incorporating prestigious colleges such as Central College and the University Visvesvaraya College of Engineering, which served as its nucleus. <br><br>
+            
+            From its inception, Bangalore University has prioritized academic excellence and innovation. In 1965-66, the university introduced Honours Courses across several disciplines, including Botany, Chemistry, Economics, and more, attracting some of the brightest minds from the region. This initiative not only provided students with advanced learning opportunities but also streamlined their pathway to postgraduate studies, allowing Honours graduates preferential access to these programs. The demand for quality education led to a substantial increase in the university's student enrollment and the diversification of its academic offerings. <br><br>
+            
+            In 1973, Bangalore University relocated to its expansive Jnana Bharathi campus, which spans 1,100 acres of lush greenery and modern facilities. This campus became a hub for various postgraduate departments, administrative offices, and student services, fostering an environment conducive to learning and research. The university houses the offices of key administrative figures, such as the Vice Chancellor and the Registrar, along with numerous departments, directorates, and centers focused on higher learning.<br><br>
+            
+            Over the decades, Bangalore University has evolved into one of the largest universities in Asia, offering a rich tapestry of academic programs across six faculties: Arts, Science, Commerce and Management, Education, Law, and Engineering. Currently, it boasts 48 postgraduate departments, one postgraduate center at Kolar, and a network of 684 affiliated colleges, catering to a diverse student population exceeding three lakh. The university offers 50 postgraduate courses and various employment-oriented diploma and certificate programs, including five-year integrated courses in biological sciences, thereby aligning its curriculum with the demands of contemporary job markets.<br><br>
+            
+            The university's commitment to quality education has garnered recognition on both national and international platforms. It was first accredited by the National Assessment and Accreditation Council (NAAC) in 2002, achieving Five Star Status, and has since been re-accredited multiple times, maintaining an 'A' Grade in subsequent evaluations. In the QS University Rankings for BRICS nations, Bangalore University was ranked 151st, showcasing its growing prominence in the global academic arena.<br><br>
+            
+            Beyond academics, Bangalore University actively engages with society through various outreach initiatives, including endowment lectures and the publication of research on contemporary issues. The establishment of centers such as Gandhi Bhavan, the Dr. B.R. Ambedkar Study and Research Centre, and the Centre for Study on Social Exclusion and Inclusive Policy reflects its commitment to social relevance and inclusivity in education. The university has also forged numerous Memorandums of Understanding (MOUs) with national and international institutions, enhancing its research capabilities and collaborative efforts.<br><br>
+            
+            With a vision to make significant contributions to higher education across diverse fields, Bangalore University emphasizes quality teaching, research with social relevance, and community engagement. Its mission is encapsulated in its emblem, "JNANAM VIGNANA SAHITHAM," which symbolizes the pursuit of knowledge and the integration of learning with practical application. As it continues to grow and adapt to the changing educational landscape, Bangalore University remains a beacon of higher learning, committed to nurturing future generations of leaders and innovators.<br><br>
+            </p>
+        </div>
+    </div>
+</body>
+</html>
+<?php
+    include("Footer.html");
+?>
---
+++ contact.php
@@ -0,0 +1,56 @@
+<?php
+    include("Header.html")
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../styles/contact.css">
+    <title>Contact - Bangalore University</title>
+</head>
+<body>
+    <div class="section1">
+        <h1>Contact Us</h1>
+        <h3>General Contact Information</h3>
+        <div class="cards">
+            <div class="card1">
+                <div class="s1 card">
+                    <img src="assets/call-black.png" alt="call">
+                    <h3>TELEPHONE</h3>
+                </div>
+                <hr>
+                <div class="r2 card">
+                    <h3>Campus Operator</h3>
+                    <p>1800-3996-4123</p>
+                </div>
+            </div>
+            <div class="card2">
+                <div class="s2 card">
+                    <img src="assets/email-black.png" alt="mail">
+                    <h3>E-MAIL</h3>
+                </div>
+                <hr>
+                <div class="r2 card">
+                    <h3>Mail To</h3>
+                    <p>bangaloreuniversity.edu@gmail.com</p>
+                </div>
+            </div>
+            <div class="card3">
+                <div class="s3 card">
+                    <img src="assets/pin-black.png" alt="location">
+                    <h3>LOCATION</h3>
+                </div>
+                <hr>
+                <div class="r3 card">
+                    <h3>Primary Address</h3>
+                    <p>Bangalore University, Jnana Bharathi Campus, Mysore Road, Bengaluru, Karnataka 560056</p>
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
+<?php
+    include("Footer.html")
+?>
